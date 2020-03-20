e079014d8767b7458e628bb5ffc6b2f30f86ce16
NaoyaTsudome <noy.dmt1021@gmail.com>
2020-03-20 11:38:51 +0000

    新規ユーザー登録のバリデーション追加

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 15 }
+  validates :email, presence: true, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
+  validates :password, presence: true, length: { minimum: 8, maximum: 32 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,}+\z/i }
+
+  has_secure_password
+end
