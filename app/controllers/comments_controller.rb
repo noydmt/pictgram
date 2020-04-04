7c19fc1d5e2a0001b7235494b9552ac7867000fd
NaoyaTsudome <noy.dmt1021@gmail.com>
2020-04-04 21:26:12 +0900

    create comment controller,model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,26 @@
 class CommentsController < ApplicationController
+
+  def index
+    @comments = Comment.find_by(user_id: current_user.id)
+  end
+
+  def create
+    comment = Comment.new(params_comments)
+    if comment.save
+      redirect_to topics_path, success: 'コメントの投稿に成功しました'
+    else
+      flash.now[:danger] = 'コメントの投稿に失敗しました'
+      redirect_to topics_path
+    end
+   end
+
+  def destroy
+    
+  end
+  
+  private
+  def params_comments
+    params.require(:comment).permit(:topic_id, :content, :user_id)
+  end
+
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,9 @@
 class Comment < ApplicationRecord
+
+  validates :content, presence: true , length: { maximum: 140 }
+  #validates: user_id, presence: true
+  #validates: topic_id, presence: true
+
+  belongs_to :topic 
+  belongs_to :user
 end
